749afc6959988873b48f94c92e8cea0fe0e61531
Marcos Chaves <marcosanf.c@gmail.com>
2021-07-10 18:15:44 -0300

    create login page

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              height: size.height * 0.36,
+              width: size.width,
+            ),
+            Positioned(
+              top: 37,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                AppImages.person,
+                width: 208,
+                height: 350,
+              ),
+            ),
+            Positioned(
+              bottom: size.height * 0.12,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text(
+                      "Organize seusboletos em um só lugar",
+                      style: TextStyles.titleHome,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print("Clicou :D");
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
